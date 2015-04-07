5c2c84316fd81067035db1a0dfa9a45c1e51bc45
Eduard Carreras <ecarreras@gisce.net>
2015-04-07 13:43:58 +0200

    Add script

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='destral',
     version='0.1.0',
-    packages=['destral'],
+    packages=find_packages(),
     url='https://github.com/gisce/destral',
     install_requires=['osconf'],
     license='MIT',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
+    entry_points={
+        'console_scripts': [
+            'ootest = destral.testing:main'
+        ]
+    },
     description=''
 )
