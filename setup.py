4bc249fccf4d846f53904c431a302af82bb2f376
Eduard Carreras <ecarreras@gisce.net>
2015-04-16 10:25:04 +0200

    Fix dup install_requires

--- setup.py
+++ setup.py
@@ -6,7 +6,10 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     url='https://github.com/gisce/destral',
-    install_requires=['osconf'],
+    install_requires=[
+        'osconf',
+        'expects'
+    ],
     license='MIT',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
@@ -15,8 +18,5 @@ setup(
             'ootest = destral.cli:main'
         ]
     },
-    install_requires=[
-        'expects'
-    ],
     description=''
 )
