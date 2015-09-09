6caac899c4f2efac1a2a7398d5533d6899f73a1a
Eduard Carreras <ecarreras@gisce.net>
2015-09-09 12:49:30 +0200

    Add a method to get de dependencies

--- destral/utils.py
+++ destral/utils.py
@@ -1,3 +1,4 @@
+from ast import literal_eval
 import imp
 import os
 
@@ -37,3 +38,33 @@ def module_exists(module):
             if openfile:
                 openfile.close()
                 return True
+
+
+def get_dependencies(module, addons_path=None, deps=None):
+    if deps is None:
+        deps = []
+    if addons_path is None:
+        from destral.openerp import OpenERPService
+        service = OpenERPService()
+        addons_path = service.config['addons_path']
+    pj = os.path.join
+    module_path = pj(addons_path, module)
+    if not os.path.exists(module_path):
+        raise Exception('Module {} not found in {}'.format(
+            module, addons_path
+        ))
+    terp_path = pj(module_path, '__terp__.py')
+    if not os.path.exists(terp_path):
+        raise Exception(
+            'Module {} is not a valid module. Missing __terp__.py file'.format(
+                module
+            )
+        )
+    with open(terp_path, 'r') as terp_file:
+        terp = literal_eval(terp_file.read())
+
+    for dep in terp['depends']:
+        deps.append(dep)
+        deps += get_dependencies(dep, addons_path, deps)
+
+    return list(set(deps))
