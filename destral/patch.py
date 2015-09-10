8544d24fc0f236e4d97617d4feab2b1e5362239e
Eduard Carreras <ecarreras@gisce.net>
2015-09-10 15:50:01 +0200

    Remove non patched openerp logger

--- destral/patch.py
+++ destral/patch.py
@@ -2,5 +2,5 @@ def patch_root_logger():
     import logging
     if 'openerp' not in logging.Logger.manager.loggerDict:
         logger = logging.getLogger()
-        logger.propagate = False
-        print logging.Logger.manager.loggerDict
+        logger.handlers = []
+
