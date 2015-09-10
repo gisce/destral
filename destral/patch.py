7624be3aa10bffb454df8537d63bae44c3622b9f
Eduard Carreras <ecarreras@gisce.net>
2015-09-10 15:18:45 +0200

    who knows it...

--- destral/patch.py
+++ destral/patch.py
@@ -3,3 +3,4 @@ def patch_root_logger():
     if 'openerp' not in logging.Logger.manager.loggerDict:
         logger = logging.getLogger()
         logger.propagate = False
+        print logging.Logger.manager.loggerDict
