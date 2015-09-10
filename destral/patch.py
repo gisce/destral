2611710a515e1f1502e1da4af59bf245278171c6
Eduard Carreras <ecarreras@gisce.net>
2015-09-10 17:07:07 +0200

    Reset to basicConfig()

--- destral/patch.py
+++ destral/patch.py
@@ -3,4 +3,5 @@ def patch_root_logger():
     if 'openerp' not in logging.Logger.manager.loggerDict:
         logger = logging.getLogger()
         logger.handlers = []
+        logging.basicConfig()
 
