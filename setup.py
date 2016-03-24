bc7f91ce6adf294d33e73972887fe2e8700b9114
Eduard Carreras <ecarreras@gisce.net>
2016-03-24 11:05:38 +0100

    Fucking version for Pypi nightmares

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='destral',
-    version='0.6.2',
+    version='0.6.3',
     packages=find_packages(),
     url='https://github.com/gisce/destral',
     install_requires=[
