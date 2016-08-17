7233c4939a54344c5114c0276e741ba96a13c4b2
Eduard Carreras <ecarreras@gisce.net>
2016-08-17 11:38:46 +0200

    Bump to v0.13.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='destral',
-    version='0.13.0',
+    version='0.13.1',
     packages=find_packages(),
     url='https://github.com/gisce/destral',
     install_requires=[
