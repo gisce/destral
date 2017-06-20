532f1f70c0149adc861cc653326de7118faae292
Jaume Florez <jflorez@gisce.net>
2017-06-20 12:54:15 +0200

    Update import with specific-import for utils test
    - Also add case of no file found to test

--- spec/utils_spec.py
+++ spec/utils_spec.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 
-from destral.utils import *
+from destral.utils import find_files, compare_pofiles
 from expects import *
 
 from .fixtures import get_fixture
@@ -29,17 +29,23 @@ with description('Translations'):
             pathA = get_fixture('potfileA.pot')
             pathB = get_fixture('potfileB.pot')  # Has the same msg ids than A
             pathC = get_fixture('potfileC.pot')  # Has 2 less messages than A
+            pathD = get_fixture('potfileD.pot')  # Does not exist
             missing_msg = compare_pofiles(pathA, pathB)
             expect(missing_msg).to(equal(0))
             missing_msg = compare_pofiles(pathA, pathC)
             expect(missing_msg).to(equal(2))
+            missing_msg = compare_pofiles(pathA, pathD)
+            expect(missing_msg).to(equal(-1))
 
         with it('must check all msg in first po file to be translated in '
                 'the second one'):
             pathA = get_fixture('potfileA.pot')
             pathB = get_fixture('pofileA.po') # Has all messages translated
             pathC = get_fixture('pofileB.po') # Has 1 message untranslated
+            pathD = get_fixture('pofileC.po') # Does not exist
             untranslated_msg = compare_pofiles(pathA, pathB, True)
             expect(untranslated_msg).to(equal(0))
             untranslated_msg = compare_pofiles(pathA, pathC, True)
             expect(untranslated_msg).to(equal(1))
+            untranslated_msg = compare_pofiles(pathA, pathD, True)
+            expect(untranslated_msg).to(equal(-1))
