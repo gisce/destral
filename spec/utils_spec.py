84f0de4da73b34fa547d1b3548d082ae8b3c849b
Jaume Florez <jflorez@gisce.net>
2017-06-20 12:18:29 +0200

    Add translations' compare_pofiles tests

--- spec/utils_spec.py
+++ spec/utils_spec.py
@@ -22,3 +22,24 @@ with description('With a diff'):
             'requirements.txt',
             'addons/foo/bar.txt'
         ))
+
+with description('Translations'):
+    with context('Comparing pofiles'):
+        with it('must check all msg in first po file to be in the second one'):
+            pathA = get_fixture('potfileA.pot')
+            pathB = get_fixture('potfileB.pot')  # Has the same msg ids than A
+            pathC = get_fixture('potfileC.pot')  # Has 2 less messages than A
+            missing_msg = compare_pofiles(pathA, pathB)
+            expect(missing_msg).to(equal(0))
+            missing_msg = compare_pofiles(pathA, pathC)
+            expect(missing_msg).to(equal(2))
+
+        with it('must check all msg in first po file to be translated in '
+                'the second one'):
+            pathA = get_fixture('potfileA.pot')
+            pathB = get_fixture('pofileA.po') # Has all messages translated
+            pathC = get_fixture('pofileB.po') # Has 1 message untranslated
+            untranslated_msg = compare_pofiles(pathA, pathB, True)
+            expect(untranslated_msg).to(equal(0))
+            untranslated_msg = compare_pofiles(pathA, pathC, True)
+            expect(untranslated_msg).to(equal(1))
