97d1a8cc50b2bfab93a66e80cbc8fbc8c7dc4e87
Jaume Florez <jflorez@gisce.net>
2017-06-23 09:14:11 +0200

    Fix test comparing POTs, comparing trans too
    - POT files are always untranslated

--- spec/utils_spec.py
+++ spec/utils_spec.py
@@ -31,22 +31,22 @@ with description('Translations'):
             missing_msg, untranslated_msg = compare_pofiles(pathA, pathB)
             expect(untranslated_msg).to(equal(0))
             expect(missing_msg).to(equal(0))
-        with it('Compare POT files with one POT missing 2 strings'):
+        with it('Compare PO files with one PO missing'
+                ' 2 strings and 1 untranslated'):
             pathA = get_fixture('potfileA.pot')
             pathC = get_fixture('potfileB.pot')  # Has 2 less messages than A
             missing_msg, untranslated_msg = compare_pofiles(pathA, pathC)
-            expect(untranslated_msg).to(equal(0))
+            expect(untranslated_msg).to(equal(1))
             expect(missing_msg).to(equal(2))
-        with it('Compare POT files with one missing POT file'):
-            pathA = get_fixture('potfileA.pot')
-            pathD = get_fixture('potfileC.pot')  # Does not exist
-            missing_msg, untranslated_msg = compare_pofiles(pathA, pathD)
-            expect(untranslated_msg).to(equal(-1))
-            expect(missing_msg).to(equal(-1))
-
         with it('Compare POT and PO files missing 1 translation'):
             pathA = get_fixture('potfileA.pot')
             pathC = get_fixture('pofileB.po')  # Has 1 message untranslated
             missing_msg, untranslated_msg = compare_pofiles(pathA, pathC)
             expect(missing_msg).to(equal(0))
             expect(untranslated_msg).to(equal(1))
+        with it('Compare POT files with one missing POT file'):
+            pathA = get_fixture('potfileA.pot')
+            pathD = get_fixture('potfileC.pot')  # Does not exist
+            missing_msg, untranslated_msg = compare_pofiles(pathA, pathD)
+            expect(untranslated_msg).to(equal(-1))
+            expect(missing_msg).to(equal(-1))
