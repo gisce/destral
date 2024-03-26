82f258651bfdcdea0057a031d3bc54f34ab51fe6
giscegit <devel@gisce.net>
2024-03-26 16:10:43 +0000

    Bump to v1.18.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
     requirements.append('mamba>=0.11.0')
 setup(
     name='destral',
-    version='1.17.0',
+    version='1.18.0',
     packages=find_packages(),
     url='https://github.com/gisce/destral',
     install_requires=requirements,
