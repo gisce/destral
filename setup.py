9045cf3519a108b72f914cd93ef3f74ce611c1ab
giscegit <devel@gisce.net>
2026-05-04 08:42:53 +0000

    Bump to v2.3.4

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ else:
 
 setup(
     name='destral',
-    version='2.3.3',
+    version='2.3.4',
     packages=find_packages(),
     url='https://github.com/gisce/destral',
     install_requires=requirements,
